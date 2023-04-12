f8f2cdcc4017534f6091412d92c69487ec0751ea
SadRus <rystixx@yandex.ru>
2023-04-12 22:56:02 +0300

    Using functools partial for tg callback

--- tg_bot.py
+++ tg_bot.py
@@ -5,6 +5,7 @@ import telegram
 from create_parser import create_parser
 from detect_intent_text import detect_intent_texts
 from dotenv import load_dotenv
+from functools import partial
 from telegram import Update
 from telegram.ext import (
     CallbackContext,
@@ -27,9 +28,6 @@ def start(update: Update, context: CallbackContext):
 
 
 def send_dialogflow_answer(update: Update, context: CallbackContext):
-    project_id = os.getenv('PROJECT_ID')
-    session_id = os.getenv('TG_CHAT_ID')
-
     input_text = update.message.text
     intent_content = detect_intent_texts(input_text, session_id, project_id)
     output_text = intent_content['fulfillment_text']
@@ -47,6 +45,11 @@ def main():
     tg_bot_token = os.getenv('TG_BOT_TOKEN')
     tg_bot_logger_token = os.getenv('TG_BOT_LOGGER_TOKEN')
     tg_chat_id = os.getenv('TG_CHAT_ID')
+    send_dialogflow_answer_partial = partial(
+        send_dialogflow_answer,
+        project_id=os.getenv('PROJECT_ID'),
+        session_id=os.getenv('TG_CHAT_ID'),
+    )
 
     tg_bot_logger = telegram.Bot(token=tg_bot_logger_token)
 
@@ -78,7 +81,7 @@ def main():
     dispatcher.add_handler(
         MessageHandler(
             Filters.text & (~Filters.command),
-            send_dialogflow_answer,
+            send_dialogflow_answer_partial,
         )
     )
     try:
